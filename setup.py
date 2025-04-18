4a427bde619f0e5f04b212c431aa444e2bc034e9
wznln <18435084+Xuwznln@users.noreply.github.com>
2025-04-18 15:16:00 +0800

    fix: missing yaml during installation

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 from setuptools import setup, find_packages
-from glob import glob
-import os
 
 package_name = 'unilabos'
 
@@ -8,21 +6,13 @@ setup(
     name=package_name,
     version='0.8.0',
     packages=find_packages(),
-    # data_files=[
-    #     ('share/ament_index/resource_index/packages',
-    #         ['resource/' + package_name]),
-    #     ('share/' + package_name, ['package.xml']),
-    #     # (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-    #     # (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
-    #     # (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
-    #     # (os.path.join('share', package_name, 'config'), glob('config/*'))
-    # ],
+    include_package_data=True,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Junhan Chang',
     maintainer_email='changjh@pku.edu.cn',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='',
+    license='GPL v3',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
